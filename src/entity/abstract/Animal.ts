88ca8a7d64bffa0f66a68cf8224f461c2ff70984
Garrett Vance <garrettsvance@gmail.com>
2024-10-09 12:44:14 -0600

    created generic functions within animal.ts

--- src/entity/abstract/Animal.ts
+++ src/entity/abstract/Animal.ts
@@ -2,8 +2,23 @@ export abstract class Animal {
   name: string;
   trainingPriority: number;
 
-  constructor(name: string, trainingPriority: number) {
+  protected constructor(name: string, trainingPriority: number) {
     this.trainingPriority = trainingPriority;
     this.name = name;
   }
+
+  static getSorted<T extends Animal>(animalList: T[]): T[] {
+    return animalList.sort((animal1, animal2) =>
+        animal1.trainingPriority < animal2.trainingPriority ? -1 : 1
+    );
+  }
+  static getTrainingPriorityList<T extends Animal>(animalList: T[]): string {
+    return animalList
+        .map(
+            (animal) =>
+                animal.name + "'s training priority: " + animal.trainingPriority + "\n"
+        )
+        .join("");
+  }
+
 }
